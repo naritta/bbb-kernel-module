77ff351abf6bb928d63550a602530ffc288489ac
RittaNarita <narittan@gmail.com>
2014-12-16 23:47:40 +0900

    add gitignore and validate debug

--- drivers/io.c
+++ drivers/io.c
@@ -1,12 +1,26 @@
 
-int io_init(void){}
+#include <stdio.h>
+
+int io_init(void){
+    return 0;
+}
 
 void io_exit(void){}
 
-int io_read(){}
+int io_read(){
+    return 0;
+}
 
-int io_write(){}
+int io_write(){
+    return 0;
+}
 
-int set_button(){}
+int set_button(){
+    return 0;
+}
 
 void set_light(){}
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
